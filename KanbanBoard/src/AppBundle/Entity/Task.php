107f8dd631278f80b9a7637e9c92f527d4d35b06
pavlinazgurova <pavlina.zgurova@abv.bg>
2017-08-28 13:04:36 +0300

    Added a KanbanBoardApp.

---
+++ KanbanBoard/src/AppBundle/Entity/Task.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Task
+ *
+ * @ORM\Table(name="tasks")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\TaskRepository")
+ */
+class Task
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=2550)
+     * @NotBlank
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=20)
+     * @NotBlank
+     */
+    private $status;
+
+    /**
+     * @return int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Task
+     */
+    public function setId(?int $id): Task
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Task
+     */
+    public function setTitle(?string $title): Task
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     * @return Task
+     */
+    public function setStatus(?string $status): Task
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+
+}
+
